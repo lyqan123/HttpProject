b9908ce60db7cffbfdf54333aa744c4b6ba341e1
lyqan123 <945793923@qq.com>
2019-07-08 10:43:52 +0800

    second commit

---
+++ src/com/bawei/Project.java
@@ -0,0 +1,24 @@
+package com.bawei;
+
+public class Project {
+
+    private String name;
+
+    private Integer password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
+}
